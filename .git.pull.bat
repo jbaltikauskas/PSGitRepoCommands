80e81f15008b941ba13c21f60ffb3db4ad150129
jbaltikauskas <baltikauskas@gmail.com>
2026-09-23 22:16:08 -0400

    chore: force submodule checkout in git maintenance scripts
    Discard local submodule changes during pull and reset workflows.

--- .git.pull.bat
+++ .git.pull.bat
@@ -4,6 +4,6 @@ git pull
 
 git submodule sync --recursive
 
-git submodule update --init --recursive
+git submodule update --init --recursive --force
 
 git gc
--- .git.reset.bat
+++ .git.reset.bat
@@ -6,6 +6,6 @@ git pull
 
 git submodule sync --recursive
 
-git submodule update --init --recursive
+git submodule update --init --recursive --force
 
 git gc
