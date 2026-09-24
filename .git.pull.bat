2d8479b32d5823973696e0aa1fd6b45242a5a478
jbaltikauskas <baltikauskas@gmail.com>
2026-09-23 21:58:47 -0400

    chore: sync submodules in git maintenance batch scripts
    Ensure pull and reset workflows initialize and update nested submodules.

--- .git.pull.bat
+++ .git.pull.bat
@@ -2,4 +2,8 @@ git fetch --prune --all
 
 git pull
 
+git submodule sync --recursive
+
+git submodule update --init --recursive
+
 git gc
--- .git.reset.bat
+++ .git.reset.bat
@@ -4,4 +4,8 @@ git reset --hard
 
 git pull
 
+git submodule sync --recursive
+
+git submodule update --init --recursive
+
 git gc
